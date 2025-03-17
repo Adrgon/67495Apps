71be9b164e4593841aa38f7e4033567ab97b3c19
Gonzalez Adrian <adrgon@gmail.com>
2025-03-17 19:37:05 -0300

    affterclass 5

---
+++ affterclass05/ecomerce/src/databases/users.js
@@ -0,0 +1,2 @@
+export const apiKey = "AIzaSyA0nS3tCmN2OgOS1GCAr4Gf80bP609LNEU";
+export const baseAuthUrl = "https://identitytoolkit.googleapis.com/v1";
---
+++ prueba/ecomerce/App.js
@@ -0,0 +1,36 @@
+import {useFonts} from 'expo-font'
+import Navigator from './src/navigation/Navigator';
+import { SafeAreaView, View, StyleSheet, Platform, StatusBar } from 'react-native';
+import { Provider } from 'react-redux';
+import store from './src/store'
+import { colors } from './src/global/color';
+
+
+export default function App() {
+
+  const [fontsLoaded, fontError] = useFonts({
+    Josefin: require("./assets/JosefinSans-Regular.ttf"),
+  })
+
+  if(!fontsLoaded || fontError) {
+    return null;
+  }
+
+  if(fontsLoaded && !fontError) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Provider store={store}>
+          <Navigator />
+        </Provider>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    flex: 1,
+    backgroundColor: colors.primary,
+  },
+});
